d8122a9fc874de4a873268c40063545c12fdc0d5
yalh76 <yalh@yahoo.com>
2020-06-17 00:02:57 +0200

    Adding pythonz dependencies

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="python-pip python-virtualenv python-dev uwsgi uwsgi-plugin-python"
+pkg_dependencies="python-pip python-virtualenv python-dev uwsgi uwsgi-plugin-python build-essential zlib1g-dev libbz2-dev libssl-dev libreadline-dev libncurses5-dev libsqlite3-dev libgdbm-dev libdb-dev libexpat-dev libpcap-dev liblzma-dev libpcre3-dev libffi-dev"
 
 python_version=2.7.2
 
