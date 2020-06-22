6bcbfb9dcc482ed2f2644479079fc4e2110e471a
yalh76 <yalh@yahoo.com>
2020-06-22 05:35:15 +0200

    Switch to ynh_install_python

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,9 +5,10 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="python-pip python-virtualenv python-dev uwsgi uwsgi-plugin-python build-essential zlib1g-dev libbz2-dev libssl-dev libreadline-dev libncurses5-dev libsqlite3-dev libgdbm-dev libdb-dev libexpat-dev libpcap-dev liblzma-dev libpcre3-dev libffi-dev"
+pkg_dependencies=""
+#python-pip python-virtualenv python-dev uwsgi uwsgi-plugin-python
 
-python_version=2.7.2
+PHYTON_VERSION=2.7.18
 
 #=================================================
 # PERSONAL HELPERS
